ad985ac8005a2dfd924d8876ccb3500f16470766
Tin Huynh <vantintttp@gmail.com>
2016-11-11 15:22:16 +0700

    Update predix_authen.sh

--- predix_authen.sh
+++ predix_authen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget -q -O - "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar xz
 sudo mv cf /usr/local/bin/
-/usr/local/bin/cf login -a https://api.system.aws-usw02-pr.ice.predix.io -u tinhv@fsoft.com.vn -p Th1enL0ngB@tB0
+/usr/local/bin/cf login -a https://api.system.aws-usw02-pr.ice.predix.io -u tinhv@example -p password
 /usr/local/bin/cf push -f manifest.yml
